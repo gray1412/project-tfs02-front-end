607867557bde0fc658d1c657c985f3641dd7a7dd
luongduyanh <luongduyanh1999@gmail.com>
2021-07-16 12:30:34 +0700

    update vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import account from "./account";
+import product from "./product";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    account,
+    product,
+  },
 });
